39c55c4305e9e547b77e9329b2ec756f76656396
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-04-20 15:03:09 -0300

    set up with typescript and styled components

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border - box;
+        font-family: 'Titillium Web', sans-serif;
+    }
+
+    body {
+        background: #f2f3f5;
+    }
+`
